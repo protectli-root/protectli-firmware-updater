c2631f8a06b8ce71f4a9e650209cb586cc28555a
Ed Borgquist IV <ed.borgquist@protectli.com>
2021-03-17 23:50:53 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,5 +7,6 @@ rm -rf bios-flash-master/
 rm -rf bios-flash-master.zip
 wget -q https://gitlab.protectli.com/protectli/bios-flash/-/archive/master/bios-flash-master.zip
 unzip -q bios-flash-master.zip
-cd bios-flash-master/
-./main.py
+echo "BIOS Flash is ready. Run the following command as root to execute BIOS Flash:"
+echo "  /tmp/bios-flash-master/main.py"
+exit 0
