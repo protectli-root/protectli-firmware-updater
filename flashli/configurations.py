b434d813e1de08f2b1fa317c2c1d814d51e59233
SergeyKonst3 <79882629+SergeyKonst3@users.noreply.github.com>
2022-01-25 16:56:14 -0800

    Update configurations.py

--- flashli/configurations.py
+++ flashli/configurations.py
@@ -78,20 +78,6 @@ CONFIGURATIONS = types.MappingProxyType({
         ],
         'command': flash_command,
     },
-    'fw6a': {
-        'cpu': '3867U',
-        'bios': [
-            {
-                'vendor': 'ami',
-                'file': 'FW6_all_YKBR6L12.bin',
-            },
-            {
-                'vendor': 'coreboot',
-                'file': 'protectli_all_fw6_DF_v1.0.12.rom',
-            },
-        ],
-        'command': flash_command,
-    },
     'fw6c': {
         'cpu': '7200U',
         'bios': [
